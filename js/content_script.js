733275ea1dd5edd9db54c0c260c0884b6438153b
Seva Ivanov <seva@tumahn.net>
2016-12-11 20:17:55 -0500

    cleanup

--- js/content_script.js
+++ js/content_script.js
@@ -1,6 +1,8 @@
 var running = false;
 
-// world.grid = [height, width] = [rows, columns]
+var body = document.getElementsByTagName('body')[0];
+
+// world.grid has [height, width] <=> [rows, columns]
 var world,
     door = {};
 
@@ -11,24 +13,22 @@ var worldLinks = new Array(new Array()),
 var pageWords,
     pageLetters = new Array(new Array());
 
-// sarah
-
-var body = document.getElementsByTagName('body')[0];
-
-// Variable that counts the number of creature
-var count = 0;
-
-var creature = {};
+var capturedLetters = [];
 
 var creatureY = 0,
-    creatureX = 0,
-    creatureXPixel = 0, // TODO to implement more smooth motions
-    creatureYPixel = 0;
+    creatureX = 0;
 
+// cellauto.js library
 function buildWorld()
 {
+    /* To get a world of the size of the document the height / width needs
+     * to be determined on window load considering the dynamically modified css.
+     * Therefore, the world will need to be created there too. However, on
+     * window load did not work for me. Here is how a height would be
+     * calculated:
+     *      height: Math.ceil($(document).width() / 64)
+     */
 	world = new CAWorld({
-		//height: Math.ceil($(document).height() / 64), // on window load...
 		height: 20, // columns
 		width: 30,  // rows
 		cellSize: 64
@@ -58,85 +58,67 @@ function buildWorld()
 	world.initialize([
 		{ name: 'wall', distribution: 100 }
 	]);
-
-    //console.log(world);
-    //console.log(world.grid[0][0]);
-    //console.log(world.grid[0][0].open);
 }
 
 function setup()
 {
-    chrome.runtime.sendMessage({name: "isPaused?"}, function (response) {
-
-    if (response.value != 'true')
+    chrome.runtime.sendMessage({name: "isPaused?"}, function (response)
     {
-      running = true;
+        if (response.value != 'true')
+        {
+            running = true;
 
-      // force it since height is not always 100% of the document
-      p5Canvas = createCanvas($(document).width(), $(document).height());
+            // force it since height is not always 100% of the document
+            p5Canvas = createCanvas($(document).width(), $(document).height());
 
-      p5Canvas.elt.style.position = 'absolute';
-      p5Canvas.elt.style.top = 0;
-      p5Canvas.elt.style.left = 0;
-      p5Canvas.elt.style["z-index"] = 1000;
-      p5Canvas.elt.style["pointer-events"] = 'none';
+            p5Canvas.elt.style.position = 'absolute';
+            p5Canvas.elt.style.top = 0;
+            p5Canvas.elt.style.left = 0;
+            p5Canvas.elt.style["z-index"] = 1000;
+            p5Canvas.elt.style["pointer-events"] = 'none';
 
-      // init chars
-      for (var evol = 1; evol < 5; evol++)
-      {
-          creature[evol] = {
-            "left" : {},
-            "right": {}
-          };
-      }
+            door["white"] = loadImage(
+                chrome.extension.getURL("images/whiteDoor.png")
+            );
+            door["black"] = loadImage(
+                chrome.extension.getURL("images/blackDoor.png")
+            );
 
-      door["white"] = loadImage(
-          chrome.extension.getURL("images/whiteDoor.png")
-      );
-      door["black"] = loadImage(
-          chrome.extension.getURL("images/blackDoor.png")
-      );
+            $('*').css({'cursor': 'none'});
 
-      creature[4]["left"] = loadImage(
-          chrome.extension.getURL("images/sprite04Left.png")
-      );
-      creature[4]["right"] = loadImage(
-          chrome.extension.getURL("images/sprite04Right.png")
-      );
+            buildWorld();
 
-      $('*').css({'cursor': 'none'});
+            initArrays();
 
-      buildWorld();
-      initArrays();
-      extractPageText();
+            extractPageText();
 
-      lastKeyPressedTimer = millis();
+            lastKeyPressedTimer = millis();
 
-      linkDiv = document.createElement('div');
-      linkDiv.id = 'p5LinkDiv';
-      $(linkDiv).appendTo(body);
+            // bottom screen url display
+            linkDiv = document.createElement('div');
+            linkDiv.id = 'p5LinkDiv';
+            $(linkDiv).appendTo(body);
 
-      $('#p5LinkDiv').css({
-        position: "fixed",
-        width: "100%",
-        height: "auto",
-        left: 0,
-        bottom: 0,
-        margin: "auto",
-        background: "black",
-        opacity: 0.9,
-        "z-index": 30000,
-        "font-size": "14pt",
-        color: "rgb(68, 154, 254)"
-      });
-    }
-    else {
-      noLoop();
-    }
-  });
+            $('#p5LinkDiv').css({
+                position: "fixed",
+                width: "100%",
+                height: "auto",
+                left: 0,
+                bottom: 0,
+                margin: "auto",
+                background: "black",
+                opacity: 0.9,
+                "z-index": 30000,
+                "font-size": "14pt",
+                color: "rgb(68, 154, 254)"
+            });
+        }
+        else
+        {
+            noLoop();
+        }
+    });
 }
-
-var lastKeyPressedTimer;
 
 function draw()
 {
@@ -150,18 +132,11 @@ function draw()
         foundLinks = loadLinks();
     }
 
-    //gameOver(); // is it?
-
     background('#ffffff');
     drawMap();
 
-    //evolveCharacter();
-
     // draws chacacter
-    isJumping = false;// addGravity();
-    keyPressed(isJumping);
-
-    drawMouse();
+    keyPressed();
 
     cellContentInteraction();
 
@@ -170,8 +145,6 @@ function draw()
         scrollTop: creatureY * world.cellSize - 100,
     }, 0);
 }
-
-var capturedLetters = [];
 
 function cellContentInteraction()
 {
@@ -187,7 +160,6 @@ function cellContentInteraction()
         {
             capturedLetters.push(letter);
             pageLetters[creatureX][creatureY] = '';
-            //console.log(capturedLetters);
         }
         else
         {
@@ -212,53 +184,7 @@ function cellContentInteraction()
     }
 }
 
-function gameOver()
-{
-    // out of bound
-    if (((creatureY * world.cellSize) > $(document).height()) ||
-       ((creatureX * world.cellSize) > $(document).width()))
-    {
-       location.reload();
-    }
-}
-
-function fromGround()
-{
-    var cellCount = 1;
-    while (world.grid[creatureX][creatureY + cellCount].open)
-    {
-        cellCount++;
-    }
-    return cellCount - 1;
-}
-
-function onGround()
-{
-    return (fromGround() == 0);
-}
-
-function addGravity()
-{
-    var isJumping = false;
-
-    // no ground
-    if (!onGround())
-    {
-        // fall
-      creatureY++;
-
-      var pixelX = creatureX * world.cellSize,
-          pixelY = creatureY * world.cellSize;
-
-        image(creature[4]["right"], pixelX, pixelY);
-
-        isJumping = true;
-    }
-
-    return isJumping;
-}
-
-function keyPressed(isJumping)
+function keyPressed()
 {
     // slow down keypress speed
     if (millis() - lastKeyPressedTimer < 80)
@@ -274,72 +200,58 @@ function keyPressed(isJumping)
         lastKeyPressedTimer = millis();
     }
 
-  // make sure it is faster than vertical browser scroll follow
-  var pixelStep = 30;
+    // make sure it is faster than vertical browser scroll follow
+    var pixelStep = 30;
 
-  // interacts with links
-  if (keyIsDown (ENTER))
-  {
-      var href = worldLinks[creatureX][creatureY];
-      if (href != '')
-      {
-          document.location.href = worldLinks[creatureX][creatureY];
-      }
-  }
-  try
-  {
-      // draws chacacter
-      if (keyIsDown (LEFT_ARROW) &&
-          world.grid[creatureX - 1][creatureY].open)
-      {
-        creatureX--;
-      }
-      else if (keyIsDown (RIGHT_ARROW) &&
-          world.grid[creatureX + 1][creatureY].open)
+    // interacts with links
+    if (keyIsDown (ENTER))
+    {
+        var href = worldLinks[creatureX][creatureY];
+        if (href != '')
+        {
+            document.location.href = worldLinks[creatureX][creatureY];
+        }
+    }
+    try
+    {
+        // draws chacacter
+        if (keyIsDown (LEFT_ARROW) &&
+        world.grid[creatureX - 1][creatureY].open)
+        {
+            creatureX--;
+        }
+        else if (keyIsDown (RIGHT_ARROW) &&
+        world.grid[creatureX + 1][creatureY].open)
+        {
+            creatureX++;
+        }
+        else if (keyIsDown (UP_ARROW) &&
+        world.grid[creatureX][creatureY - 1].open)
+        {
+            creatureY--;
+        }
+        else if (keyIsDown (DOWN_ARROW) &&
+        world.grid[creatureX][creatureY + 1].open)
+        {
+            creatureY++;
+        }
+    }
+    catch (e) // TypeError == out of the boudaries of the world map
+    {
+        return;
+    }
 
-      {
-        creatureX++;
-      }
-      // jump
-      else if (keyIsDown (UP_ARROW) &&
-              //(fromGround() < 3) &&
-              world.grid[creatureX][creatureY - 1].open)
+    var pixelX = creatureX * world.cellSize,
+    pixelY = creatureY * world.cellSize;
 
-      {
-        creatureY--;
-      }
-      else if (keyIsDown (DOWN_ARROW) &&
-          world.grid[creatureX][creatureY + 1].open)
+    // eraser
+    fill(0, 0, 0, 80);
+    //fill(225, 225, 225, 20);
+    rect(pixelX, pixelY, world.cellSize, world.cellSize);
 
-      {
-        creatureY++;
-      }
-  }
-  catch (e) // TypeError == out of the boudaries of the world map
-  {
-      return;
-  }
-
-  var pixelX = creatureX * world.cellSize,
-      pixelY = creatureY * world.cellSize;
-
-  //console.log("x,y = ", creatureX, creatureY,
-  //                      world.grid[creatureX][creatureY].open);
-  //console.log("px,py = ", pixelX, pixelY);
-
-  if (!isJumping)
-  {
-        // eraser
-        fill(0, 0, 0, 80);
-        //fill(225, 225, 225, 20);
-        rect(pixelX, pixelY, world.cellSize, world.cellSize);
-        // or..
-        //image(creature[4]["right"], pixelX, pixelY);
-  }
-
-  // move mouse on it
-  mouseX = pixelX;
-  mouseY = pixelY;
+    // move mouse on it
+    mouseX = pixelX;
+    mouseY = pixelY;
 }
 
 function loadLinks()
@@ -384,7 +296,6 @@ function loadLinks()
             }
         }
     }
-
     return found;
 }
 
@@ -441,9 +352,6 @@ function extractPageText()
             }
         }
     }
-    //console.log('pageLetter');
-    //console.log(pageLetters);
-    //console.log(pageLetters[0, 10]);
 }
 
 function drawText(l, f, x, y, h, w, lRgb, bgRgb)
@@ -467,14 +375,6 @@ function drawText(l, f, x, y, h, w, lRgb, bgRgb)
         x += (h / 5);
     }
     text(l, x, y, h, w);
-}
-
-function drawMouse()
-{
-    //noStroke();
-    fill(255,0,0);
-    //image(charImageLeft, mouseX, mouseY);
-    //rect(mouseX, mouseY, world.cellSize, world.cellSize);
 }
 
 function drawMap()
@@ -511,9 +411,6 @@ function drawMap()
                 fill(68, 154, 254);
                 rect(pixelXPos, pixelYPos, pixelHeight, pixelWidth);
                 image(door["black"], pixelXPos, pixelYPos);
-                //drawText('?', 20, pixelXPos, pixelYPos,
-                //         pixelHeight, pixelWidth,
-                //         [158, 112, 116]);
             }
         }
     }
